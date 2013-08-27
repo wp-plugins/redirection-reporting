b607f3d555b8ec6b39bc3047f00d91b52ddb9ce7
mrdenny <mrdenny@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-27 16:30:22 +0000

    Adding the 1st version of the plugin.
    git-svn-id: https://plugins.svn.wordpress.org/redirection-reporting/trunk@763159 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== Email CoAuthor On Post ===
+Contributors: mrdenny
+Donate Link: http://mrdenny.com/go/RedirectionReporting
+Tags: email
+Requires at least: 3.0.1
+Tested up to: 3.6
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Emails other people when you publish a blog post.
+
+== Description ==
+
+Allows for more details reporting for the "Redirection" plugin by John Godley.  
+This plugin will not do anything for you without using the Redirection plugin 
+to handle your 301 redirections.  This plugin was built to fix a gap in the 
+reporting which the Redirection plugin has.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `redirection-reporting.php` to the `/wp-content/plugins/redirection-reporting` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Configure the global settings through the settings page.
+
+
+== Frequently Asked Questions ==
+
+= What does this plugin do? =
+
+This plugin simply reports on the redirection logs with day by day reporting.
+
+= What date format should I be using? =
+
+You should be able to use any date format.  The PHP is going to reformat it to the
+way that MySQL wants to see it.
+
+== Screenshots ==
+
+1. A sample report
+
+
+== Changelog ==
+
+
+= 1.0 =
+* Created the report
+
+
+== Upgrade Notice ==
+
+
---
+++ redirection-reporting.php
@@ -0,0 +1,102 @@
+<?php
+/*
+Plugin Name: Redirection Reporting
+Version: 1.0
+Plugin URI: http://dcac.co/go/RedirectionReporting
+Description: Allows for more details reporting for the "Redirection" plugin by John Godley.  This plugin will not do anything for you without using the Redirection plugin to handle your 301 redirections.  This plugin was built to fix a gap in the reporting which the Redirection plugin has.
+Author: Denny Cherry
+Author URI: http://dcac.co/
+*/
+
+class redirector_reporting_class {
+
+	function activation() {
+
+		// Default options
+		$options = array (
+			'donate' => ''
+		);
+    
+		// Add options
+		add_option('redirector_reporting_options', $options);
+	 }
+
+	function tools_menu() {
+		$redirector_reporting_class = new redirector_reporting_class();
+
+		add_submenu_page('tools.php', __('Redirection Reporting', 'redirection_reporting'), __('Redirection Reporting', 'redirection_reporting'), 'manage_options', 'redirection_reporting', array($redirector_reporting_class, 'show_page'));
+	}
+
+	function show_page() {
+		global $wpdb;
+		echo "<form name='selection_form' method='post'>";
+		echo "<table border='0'><tr><td>Select URL:</td><td><select name='url'>";
+		$rows = $wpdb->get_results("select url, id from `{$wpdb->prefix}redirection_items` order by url");
+		foreach ($rows as $row) {
+			echo "<option value='";
+			echo $row->id;
+			if ($_POST['url'] == $row->id) {
+				echo "' SELECTED>";
+			} else {
+				echo "'>";
+			}
+			//echo "<option>";
+			echo $row->url;
+			echo "</option>";
+		}
+		echo "</select></td></tr>";
+		echo "<tr><td>Report Start Date:</td><td><input name='startdate' type='text' value='{$_POST['startdate']}'></td></tr>";
+		echo "<tr><td>Report End Date:</td><td><input name='enddate' type='text' value='{$_POST['enddate']}'></td></tr>";
+		echo "<tr><td colspan='2'><input type='submit' name='submit' value='Run Report'></td></tr>";
+		echo "</table></form>";
+
+		if ($_POST['url'] != "") {
+			$error = false;
+			if(strtotime($_POST['startdate']) == false) {
+				echo '<div if="message" class="error">Please enter a valid start date.<br></div>';
+				$error=true;
+			}
+			if(strtotime($_POST['enddate']) == false) {
+				echo '<div if="message" class="error">Please enter a valid end date.<br></div>';
+				$error=true;
+			}
+
+			if ($error==false) {
+				$this->draw_report($_POST['url'], $_POST['startdate'], $_POST['enddate']);
+			}
+		}
+	}
+
+	function draw_report ($id, $startdate, $enddate) {
+		global $wpdb;
+
+		$rows = $wpdb->get_results("select url, id from `{$wpdb->prefix}redirection_items` where id = $id");
+		foreach ($rows as $row) {
+			echo "Report for: $row->url";
+		}
+
+
+		$startdate = date("Y-m-d", strtotime($startdate));
+		$enddate = date("Y-m-d", strtotime($enddate));
+		$sql = "select DATE(created) AS 'dt', count(*) AS 'ct', (@curRow := @curRow + 1)%2 AS row_number FROM {$wpdb->prefix}redirection_logs JOIN    (SELECT @curRow := 0) r WHERE redirection_id = $id AND created BETWEEN '$startdate' AND '$enddate' GROUP BY DATE(created) ORDER BY DATE(created)";
+		$rows = $wpdb->get_results("$sql");
+		//echo $sql;
+		echo "<BR>";
+		echo "<table border='1' cellpadding='1' cellspacing='0'><tr><td align='center'><b>Date</b></td><td align='center'><b>Hit Count</b></td></tr>";
+		foreach ($rows as $row) {
+			echo "<tr";
+			if ($row->row_number==0) {
+				echo " bgcolor='gray'";
+			}
+			echo "><td>{$row->dt}</td><td>{$row->ct}</td></tr>";
+		}
+		echo "</table>";
+	}
+
+
+} //End Class
+
+$redirector_reporting_class = new redirector_reporting_class();
+
+register_activation_hook(__FILE__, array($redirector_reporting_class, 'activation'));
+add_action('admin_menu',  array($redirector_reporting_class, 'tools_menu'));
